ac9bea84d0677438b045d9757a6ea409f7add2ef
danilmash <danil.mashentsev@mail.ru>
2025-06-01 21:41:09 +0300

    Исправил тесты функций

--- src/utils/colorSpaces.test.ts
+++ src/utils/colorSpaces.test.ts
@@ -7,30 +7,30 @@ const color = { r: 3, g: 123, b: 5 };
 
 describe("Color Space Conversions", () => {
     it("should convert RGB to XYZ correctly", () => {
-        const [x, y, z] = rgbToXYZ(color.r, color.g, color.b);
+        const { x, y, z } = rgbToXYZ(color.r, color.g, color.b);
         expect(x).toBeCloseTo(7.1479, 2);
         expect(y).toBeCloseTo(14.196, 2);
         expect(z).toBeCloseTo(2.5069, 2);
     });
 
     it("should convert RGB to Lab correctly", () => {
-        const [l, a, b] = rgbToLab(color.r, color.g, color.b);
-        expect(l).toBeCloseTo(44.511, 2);
+        const { L, a, b } = rgbToLab(color.r, color.g, color.b);
+        expect(L).toBeCloseTo(44.511, 2);
         expect(a).toBeCloseTo(-49.781, 2);
         expect(b).toBeCloseTo(47.434, 2);
     });
 
     it("should convert RGB to LCH correctly", () => {
-        const [lchL, c, h] = rgbToLch(color.r, color.g, color.b);
-        expect(lchL).toBeCloseTo(44.511, 2);
+        const { L, c, h } = rgbToLch(color.r, color.g, color.b);
+        expect(L).toBeCloseTo(44.511, 2);
         expect(c).toBeCloseTo(68.762, 2);
         expect(h).toBeCloseTo(136.38, 2);
     });
 
     it("should convert RGB to OKLCH correctly", () => {
-        const [oklchL, oklchC, oklchH] = rgbToOklch(color.r, color.g, color.b);
-        expect(oklchL).toBeCloseTo(0.5055, 3);
-        expect(oklchC).toBeCloseTo(0.1698, 2);
-        expect(oklchH).toBeCloseTo(142.63, 1);
+        const { L, C, h } = rgbToOklch(color.r, color.g, color.b);
+        expect(L).toBeCloseTo(0.5055, 3);
+        expect(C).toBeCloseTo(0.1698, 2);
+        expect(h).toBeCloseTo(142.63, 1);
     });
 });
